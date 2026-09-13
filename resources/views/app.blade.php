3fe0105ac53826328fecb35138766fc35f521785
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-09-13 15:18:50 +0700

    fix: update favicon path to use assets directory

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/assets/favicon.ico" sizes="any">
 
     <script>
         window.__META_PAGE_VIEW_EVENT_ID = window.crypto?.randomUUID?.() ?? `${Date.now()}-${Math.random().toString(36).slice(2)}`;
